d357baa52266eabfc23d8f9e31af34f955d3e583
José Miguel Melo <josemiguelmelo94@gmail.com>
2020-06-17 16:02:36 +0100

    Create example2.sql

---
+++ database/code/example2.sql
@@ -0,0 +1,5 @@
+-- available online in file 'sample1'
+CREATE OR REPLACE
+PROCEDURE addPerson(example INTEGER, example1 INTEGER, example2 INTEGER, example3 INTEGER, example4 INTEGER,
+    birthYear pls_integer, birthMonth pls_integer, birthDate pls_integer, height pls_integer, weight pls_integer, ssn pls_integer) {
+END ADDPERSON;
